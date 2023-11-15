b144cd65624daf95eef32e898d9c821659f10d3a
Dan <sandersda@gmail.com>
2023-11-15 13:08:34 -0600

    finshed

--- src/bst.js
+++ src/bst.js
@@ -1,5 +1,83 @@
 class BinarySearchTree {
   // YOUR CODE HERE
+  constructor(value){
+    this.value = value;
+    this.left = null;
+    this.right = null;
+    this.magnitude = 1;
+  }
+  insert(newValue){
+    const newTree = new BinarySearchTree(newValue);
+    if(newValue < this.value) {
+      if(this.left === null){
+        this.left = newTree;
+      } else {
+        this.left.insert(newValue)
+      }
+    } else if(newValue > this.value) {
+      if(this.right === null) {
+        this.right = newTree;
+      } else {
+        this.right.insert(newValue);
+      }
+    }
+    this.magnitude += 1;
+  }
+  size(){
+    return this.magnitude;
+    }
+  contains(value){
+    if(value === this.value){
+      return true;
+    } else if (value < this.value && this.left !== null){
+      return this.left.contains(value);
+    } else if (value > this.value && this.right !== null){
+      return this.right.contains(value);
+    }
+    return false;
+  }
+  depthFirstForEach(callback, order = 'in-order'){
+    if (order === 'in-order') {
+      if (this.left) {
+        this.left.depthFirstForEach(callback, order);
+      }
+      callback(this.value); // in order has callback in middle
+      if (this.right) {
+        this.right.depthFirstForEach(callback, order);
+      }
+    } else if (order === 'pre-order') {
+      callback(this.value); // pre order has callback at top
+      if (this.left) {
+        this.left.depthFirstForEach(callback, order);
+      }
+      if (this.right) {
+        this.right.depthFirstForEach(callback, order);
+      }
+    } else if (order === 'post-order') {
+      if (this.left) {
+        this.left.depthFirstForEach(callback, order);
+      }
+      if (this.right) {
+        this.right.depthFirstForEach(callback, order);
+      }
+      callback(this.value); // post order has callback at end
+    }
+  }
+  breadthFirstForEach(callback){
+    const queue = [this];
+    while (queue.length > 0) {
+      const currentNode = queue.shift();
+      callback(currentNode.value);
+
+      if (currentNode.left) {
+        queue.push(currentNode.left);
+      }
+
+      if (currentNode.right) {
+        queue.push(currentNode.right);
+      }
+    }
+  }
 }
 
 module.exports = BinarySearchTree
--- src/linked-list.js
+++ src/linked-list.js
@@ -1,9 +1,82 @@
 class Node {
   // YOUR CODE HERE
+  constructor(value){
+    this.value = value;
+    this.next = null;
+    this.previous = null;
+  }
 }
 
 class LinkedList {
   // YOUR CODE HERE
+  constructor(){
+    this.head = null;
+    this.tail = null;
+  }
+  addToTail(value){
+    const newNode = new Node(value);
+    if(!this.tail){
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      newNode.previous = this.tail;
+      this.tail = newNode;
+    }
+  }
+  addToHead(value){
+    const newNode = new Node(value);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      newNode.next = this.head;
+      this.head.previous = newNode;
+      this.head = newNode;
+    }
+  }
+  removeHead(){
+    if(!this.head){
+      return null;
+    }
+    const removedValue = this.head.value;
+    this.head = this.head.next;
+    if (this.head) {
+      this.head.previous = null;
+    } else {
+      this.tail = null; 
+    }
+    return removedValue;
+  }
+  removeTail(){
+    if(!this.tail){
+      return null;
+    }
+    const removedValue = this.tail.value;
+    this.tail = this.tail.previous;
+    if (this.tail) {
+      this.tail.next = null;
+    } else {
+      this.head = null; 
+    }
+    return removedValue;
+  }
+  search(value){
+    let current = this.head;
+    while (current) {
+      if(typeof value === 'function'){
+        if (value(current.value)) {
+          return current.value;
+        }
+      } else {
+        if (current.value === value) {
+          return current.value;
+        }
+      }
+      current = current.next;
+    }
+    return null;
+  }
 }
 
 module.exports = {
